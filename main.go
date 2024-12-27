6c21da3dc588ccfacfd161c732b320b186d3ab21
Hitalo-Lima <hitalolima173@gmail.com>
2024-12-27 08:28:04 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "github.com/hitalo-lima/gopportunities/router"
+import (
+	"github.com/hitalo-lima/gopportunities/config"
+	"github.com/hitalo-lima/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
-	//initialize router
+	logger = config.GetLogger("main")
+	// initialize configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
+	// initialize router
 	router.Initialize()
 }
