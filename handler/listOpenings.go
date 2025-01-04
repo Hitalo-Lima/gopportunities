555bd5f7363797fd415b6e3d7d10698b106f1ea7
Hitalo-Lima <hitalolima173@gmail.com>
2025-01-04 18:30:01 -0300

    docs: add documentation for list openings

--- handler/listOpenings.go
+++ handler/listOpenings.go
@@ -7,6 +7,17 @@ import (
 	"github.com/hitalo-lima/gopportunities/schemas"
 )
 
+// @BasePath /api/v1
+
+// @Summary List openings
+// @Description Show all job opening
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Success 200 {object} ListOpeningsResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 404 {object} ErrorResponse
+// @Router /openings [get]
 func ListOpeningsHandler(ctx *gin.Context) {
 	openings := []schemas.Opening{}
 
